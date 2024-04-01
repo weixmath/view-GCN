30f5c5fac018991defef631c9cf916458969710b
weixmath <34408587+weixmath@users.noreply.github.com>
2024-04-01 16:02:35 +0800

    Update ImgDataset.py

--- tools/ImgDataset.py
+++ tools/ImgDataset.py
@@ -70,7 +70,87 @@ class MultiviewImgDataset(torch.utils.data.Dataset):
 
         return (class_id, torch.stack(imgs), self.filepaths[idx*self.num_views:(idx+1)*self.num_views])
 
+class MultiviewImgDataset_polar(torch.utils.data.Dataset):
+    def __init__(self, root_dir, scale_aug=False, rot_aug=False, test_mode=False, \
+                 num_models=0, num_views=20, shuffle=True):
+        self.classnames=['airplane','bathtub','bed','bench','bookshelf','bottle','bowl','car','chair',
+                         'cone','cup','curtain','desk','door','dresser','flower_pot','glass_box',
+                         'guitar','keyboard','lamp','laptop','mantel','monitor','night_stand',
+                         'person','piano','plant','radio','range_hood','sink','sofa','stairs',
+                         'stool','table','tent','toilet','tv_stand','vase','wardrobe','xbox']
+        self.root_dir = root_dir
+        self.scale_aug = scale_aug
+        self.rot_aug = rot_aug
+        self.test_mode = test_mode
+        self.num_views = num_views
+        set_ = root_dir.split('/')[-1]
+        parent_dir = root_dir.rsplit('/',2)[0]
+        self.filepaths = []
+        for i in range(len(self.classnames)):
+            all_files = sorted(glob.glob(parent_dir+'/'+self.classnames[i]+'/'+set_+'/*.png'))
+            stride = int(20/self.num_views) # 12 6 4 3 2 1
+            all_files = all_files[::stride]
 
+            if num_models == 0:
+                # Use the whole dataset
+                self.filepaths.extend(all_files)
+            else:
+                self.filepaths.extend(all_files[:min(num_models,len(all_files))])
+
+        if shuffle==True:
+            # permute
+            rand_idx = np.random.permutation(int(len(self.filepaths)/num_views))
+            filepaths_new = []
+            for i in range(len(rand_idx)):
+                filepaths_new.extend(self.filepaths[rand_idx[i]*num_views:(rand_idx[i]+1)*num_views])
+            self.filepaths = filepaths_new
+
+        if self.test_mode:
+            self.transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+            ])
+        else:
+            self.transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+            ])
+
+    def __len__(self):
+        return int(len(self.filepaths)/self.num_views)
+
+    def __getitem__(self, idx):
+        path = self.filepaths[idx*self.num_views]
+        class_name = path.split('/')[-3]
+        class_id = self.classnames.index(class_name)
+        # Use PIL instead
+        imgs = []
+        for i in range(self.num_views):
+
+            im = cv2.imread(self.filepaths[idx * self.num_views + i],0)
+            # cv2.imshow('0',im)
+            # cv2.waitKey()
+            # im = Image.open(self.filepaths[idx * self.num_views + i])
+            # im = im.load()
+            # s = im[125,125]
+            # im = Image.open(self.filepaths[idx*self.num_views+i]).convert('RGB')
+            w = im.shape[0]
+            # this will create some dead pixels
+            m = w / np.log(w * np.sqrt(2) / 2)
+            # but this may crop parts of the original img:
+            # m = w/np.log(w/2)
+            im = cv2.logPolar(im, ((w - 1.) / 2., (w - 1.) / 2.), m,
+                              cv2.INTER_LINEAR + cv2.WARP_FILL_OUTLIERS)
+            # cv2.imshow('1',im2)
+            # cv2.waitKey()
+            im= np.expand_dims(im,-1).repeat(3,axis=-1)
+            if self.transform:
+                im = self.transform(im)
+            imgs.append(im)
+
+        return (class_id, torch.stack(imgs), self.filepaths[idx*self.num_views:(idx+1)*self.num_views])
 
 class SingleImgDataset(torch.utils.data.Dataset):
 
